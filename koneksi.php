6d6656eab910a2fa783abe2a04e07be6a492c569
Muhammad Daffa Fikriawan <mdaffaf1102@gmail.com>
2024-02-03 21:17:01 +0700

    dsada

--- koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "DarkAlone07");
-if (!$koneksi) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-mysqli_select_db($koneksi, "login");
-?>
