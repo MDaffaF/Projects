214fde9c22c7b766dc984c2cb260c7ce4f4260ad
Muhammad Daffa Fikriawan <mdaffaf1102@gmail.com>
2024-02-07 08:47:03 +0700

    try

---
+++ homepage.php
@@ -0,0 +1,12 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <p>Selamt Datang <br><a href="loginpage.php">Logout</a></br></p>
+</body>
+</html>
