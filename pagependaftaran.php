4ec7b0fb90fe1427f06f79325cd3c076fceaaf70
Daffa Fikriawan <mdaffaf1102@gmail.com>
2024-02-14 19:32:33 +0700

    new

--- pagependaftaran.php
+++ pagependaftaran.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="loginpage.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <title>Login</title>
+    <title>Form Pendaftaran</title>
 </head>
 <body>
 <div class="container">
