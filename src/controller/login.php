7ede4f6fcdc31bebc1ef5110e02a32a40d7ce24f
Daffa Fikriawan <mdaffaf1102@gmail.com>
2024-02-15 23:09:07 +0700

    sweet alert

--- src/controller/login.php
+++ src/controller/login.php
@@ -1,9 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.5/dist/sweetalert2.all.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.5/dist/sweetalert2.min.css"></head>
+<body>
 <?php 
 include '../connection/koneksi.php';
- 
 $username = $_POST['username'];
 $password = $_POST['password'];
- 
 $query = mysqli_query($koneksi,"select * from tb_login where username='$username' and password='$password'");
 $cek = mysqli_num_rows($query);
 if ($cek > 0) {
@@ -11,9 +18,21 @@ if ($cek > 0) {
     exit();
 } else {
     ?>
-    <script>alert("User Tidak Ditemukan, Harap Hubungi Admin!");
-    window.location.href ="../view/loginpage.php"</script>
+    <script>Swal.fire({
+  icon: "error",
+  title: "Oops...",
+  text: "User tidak ditemukan, Harap hubungi admin!",
+  showConfirmButton: false,
+  timer: 1500
+});</script>
+  <script>
+    setTimeout(function() {
+            window.location.href = "../view/loginpage.php";
+        }, 2000); // Redirect setelah 3 detik
+    </script>
     <?php
 }
 
 ?>
+</body>
+</html>
